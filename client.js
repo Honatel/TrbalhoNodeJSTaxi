3fab337a65105e1ba1a50d34a065936d3f4e1b94
Marcos.honatel <silvahonatel@gmail.com>
2017-11-28 19:19:05 -0200

    Ajustes trabalho

--- client.js
+++ client.js
@@ -1,24 +1,25 @@
 var Client = function(){
 };
 Client.prototype.process = function(data){
+    debugger;
     processData(data);
 };
 
-function processData(allText) {
-    var record_num = 5;  // or however many elements there are in each row
-    var allTextLines = allText.split(/\r\n|\n/);
-    var entries = allTextLines[0].split(',');
-    var lines = [];
+function processData(evt) {
 
-    var headings = entries.splice(0,record_num);
-    while (entries.length>0) {
-        var tarr = [];
-        for (var j=0; j<record_num; j++) {
-            tarr.push(headings[j]+":"+entries.shift());
+    var fileArr = evt.split('\n');
+    var listObject = new Array();
+  
+    for (var i=0; i<fileArr.length; i++) {
+        var fileLine = fileArr[i].split(';');
+        if (i > 0){
+            var ObjXY = {endereco : fileLine[1], Y : fileLine[4], X : fileLine[3]};
+            
+            listObject.push(ObjXY);
         }
-        lines.push(tarr);
+               
+        }
     }
-    console.log(lines);
-}
 
 module.exports = Client;
+//node --inspect server.js (comando para debugar)
